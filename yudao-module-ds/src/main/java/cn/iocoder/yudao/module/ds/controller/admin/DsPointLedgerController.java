ac274ca9bea2192295962cb5a14eda8ca57f3a50
selfcontrol <254693270@qq.com>
2026-03-19 10:51:02 +0800

    feat(ds): enrich point ledger page with user details
    Add user nickname, mobile, source user nickname and mobile to the point ledger admin page response. This provides administrators with more context about the users involved in point transactions without requiring additional lookups.

--- yudao-module-ds/src/main/java/cn/iocoder/yudao/module/ds/controller/admin/DsPointLedgerController.java
+++ yudao-module-ds/src/main/java/cn/iocoder/yudao/module/ds/controller/admin/DsPointLedgerController.java
@@ -5,16 +5,27 @@ import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.module.ds.dal.dataobject.DsPointLedger;
+import cn.iocoder.yudao.module.ds.dal.dataobject.DsUser;
 import cn.iocoder.yudao.module.ds.dal.mysql.DsPointLedgerMapper;
-import cn.iocoder.yudao.module.ds.service.DsPointLedgerService;
+import cn.iocoder.yudao.module.ds.dal.mysql.DsUserMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
@@ -25,20 +36,52 @@ import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 public class DsPointLedgerController {
 
     @Resource
-    private DsPointLedgerService dsPointLedgerService;
-    @Resource
     private DsPointLedgerMapper dsPointLedgerMapper;
+    @Resource
+    private DsUserMapper dsUserMapper;
 
     @GetMapping("/page")
     @Operation(summary = "积分流水分页")
-    public CommonResult<PageResult<DsPointLedger>> getPage(PageParam pageParam,
-                                                            @RequestParam(value = "uid", required = false) Long uid,
-                                                            @RequestParam(value = "changeType", required = false) String changeType,
-                                                            @RequestParam(value = "bizType", required = false) String bizType) {
-        return success(dsPointLedgerMapper.selectPage(pageParam, new LambdaQueryWrapperX<DsPointLedger>()
+    public CommonResult<PageResult<DsPointLedgerAdminRespVO>> getPage(PageParam pageParam,
+                                                                       @RequestParam(value = "uid", required = false) Long uid,
+                                                                       @RequestParam(value = "changeType", required = false) String changeType,
+                                                                       @RequestParam(value = "bizType", required = false) String bizType) {
+        PageResult<DsPointLedger> ledgerPage = dsPointLedgerMapper.selectPage(pageParam, new LambdaQueryWrapperX<DsPointLedger>()
                 .eqIfPresent(DsPointLedger::getUid, uid)
                 .eqIfPresent(DsPointLedger::getChangeType, changeType)
                 .likeIfPresent(DsPointLedger::getBizType, bizType)
-                .orderByDesc(DsPointLedger::getId)));
+                .orderByDesc(DsPointLedger::getId));
+        Set<Long> userIds = ledgerPage.getList().stream()
+                .flatMap(item -> java.util.stream.Stream.of(item.getUid(), item.getSourceUid()))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+        Map<Long, DsUser> userMap = userIds.isEmpty() ? Collections.emptyMap() :
+                dsUserMapper.selectBatchIds(userIds).stream()
+                        .collect(Collectors.toMap(DsUser::getId, Function.identity(), (left, right) -> left));
+        List<DsPointLedgerAdminRespVO> result = ledgerPage.getList().stream().map(item -> {
+            DsPointLedgerAdminRespVO vo = new DsPointLedgerAdminRespVO();
+            BeanUtils.copyProperties(item, vo);
+            DsUser user = userMap.get(item.getUid());
+            if (user != null) {
+                vo.setUserNickname(user.getNickname());
+                vo.setUserMobile(user.getMobile());
+            }
+            DsUser sourceUser = userMap.get(item.getSourceUid());
+            if (sourceUser != null) {
+                vo.setSourceUserNickname(sourceUser.getNickname());
+                vo.setSourceUserMobile(sourceUser.getMobile());
+            }
+            return vo;
+        }).toList();
+        return success(new PageResult<>(result, ledgerPage.getTotal()));
+    }
+
+    @Data
+    public static class DsPointLedgerAdminRespVO extends DsPointLedger {
+
+        private String userNickname;
+        private String userMobile;
+        private String sourceUserNickname;
+        private String sourceUserMobile;
     }
 }
